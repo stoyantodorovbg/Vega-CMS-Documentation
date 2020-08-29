e2cc7d89ae301323bcff8d0cc34ab3b287f08050
stoyantodorovbg <stoyan_11@abv.bg>
2020-08-30 01:03:09 +0300

    Pages page.

--- resources/views/front/documentation/pages.blade.php
+++ resources/views/front/documentation/pages.blade.php
@@ -17,6 +17,47 @@
                 <div class="row">
                     <div class="col-12 mr-5 ml-5">
                         <h1>Pages</h1>
+                        <p>Vega CMS can renders pages from DB data.
+                            Page data can be managed via Administration or DB seeder.</p>
+                        <h2>Manage Page data via Administration</h2>
+                        <h3>Manage Page properties</h3>
+                        <p>Through the page form Status, URL, HTML classes, Title, Description, CSS styles and Meta tags can be set.</p>
+                        <img src="{{ asset('storage/documentation-screenshots/pages/admin-page-form.png') }}"
+                             alt="admin page form" class="w-75"
+                        >
+                        <p class="mt-3">The Page content can be added through Containers.</p>
+                        <img src="{{ asset('storage/documentation-screenshots/pages/page-containers-index.png') }}"
+                             alt="page containers index" class="w-75"
+                        >
+                        <p class="mt-3">Page Container can be nested into other Page Container. Its tag can be selected.
+                            Only containers which have active status are rendered.</p>
+                        <img src="{{ asset('storage/documentation-screenshots/pages/page-container-main-properties.png') }}"
+                             alt="page container main properties" class="w-75"
+                        >
+                        <p class="mt-3">Different kinds of HTML classes can be set to the container.</p>
+                        <img src="{{ asset('storage/documentation-screenshots/pages/page-container-html-classes.png') }}"
+                             alt="page container html classes" class="w-75"
+                        >
+                        <p class="mt-3">The container has four JSON fields</p>
+                        <img src="{{ asset('storage/documentation-screenshots/pages/page-container-json-fields.png') }}"
+                             alt="page container JSON fields" class="w-75"
+                        >
+                        <p class="mt-3">Though them can be added settings like these:</p>
+                        <img src="{{ asset('storage/documentation-screenshots/pages/page-container-body.png') }}"
+                             alt="page container body" class="w-75"
+                        >
+                        <p class="mt-3">When JSON field is used nested HTML tags can be created.</p>
+                        <img src="{{ asset('storage/documentation-screenshots/pages/page-container-add-json-key.png') }}"
+                             alt="page container add JSON key" class="w-75"
+                        >
+                        <img src="{{ asset('storage/documentation-screenshots/pages/page-container-json-structure.png') }}"
+                             alt="page container JSON structure" class="w-75 mt-3"
+                        >
+                        <h2>Manage page data via DB seeder</h2>
+                        <p>You can publish Vega Cms seeders reuse the code from PageTestTableSeeder to create your own pages.</p>
+                        <p><code class="command">php artisan vendor:publish --tag=vegacms-seeds</code></p>
+
+
                     </div>
                 </div>
                 <div class="row">
