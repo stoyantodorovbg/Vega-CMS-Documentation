124027a090e1afa04617b95e52166c60b7f33338
stoyantodorovbg <stoyan_11@abv.bg>
2020-09-27 12:47:16 +0300

    Tracked request added to Admin menu.

--- database/seeders/AdminSideMenuSeeder.php
+++ database/seeders/AdminSideMenuSeeder.php
@@ -15,72 +15,112 @@ class AdminSideMenuSeeder extends Seeder
     public function run()
     {
         // Sidebar navigation - admin panel - menu items
-        MenuItem::factory()->create([
-            'menu_id' => 1,
-            'parent_id' => null,
-            'status' => 1,
-            'url' => 'articles',
-            'title' => json_encode([
-                'text' => 'Articles',
+        if (!MenuItem::where('url', 'articles')->where('menu_id', 1)->first()) {
+            MenuItem::factory()->create([
+                'menu_id' => 1,
+                'parent_id' => null,
                 'status' => 1,
-                'classes' => 'icon i-page nav-item-text--light',
-                'styles' => [],
-                'structure' => [
-                    'text' => ['type' => 'text'],
-                    'status' => ['type' => 'text'],
-                    'classes' => ['type' => 'text'],
-                    'styles' => ['type' => 'json', 'nested' => []],
-                ],
-            ]),
-            'description' => json_encode([
-                'text' => '',
-                'status' => 0,
-                'classes' => '',
-                'styles' => [],
-                'structure' => [
-                    'text' => ['type' => 'text'],
-                    'status' => ['type' => 'text'],
-                    'classes' => ['type' => 'text'],
-                    'styles' => ['type' => 'json', 'nested' => []],
-                ],
-            ]),
-            'prefix' => 'admin',
-            'classes' => 'nav-item',
-            'styles' => json_encode([]),
-        ]);
+                'url' => 'articles',
+                'title' => json_encode([
+                    'text' => 'Articles',
+                    'status' => 1,
+                    'classes' => 'icon i-page nav-item-text--light',
+                    'styles' => [],
+                    'structure' => [
+                        'text' => ['type' => 'text'],
+                        'status' => ['type' => 'text'],
+                        'classes' => ['type' => 'text'],
+                        'styles' => ['type' => 'json', 'nested' => []],
+                    ],
+                ]),
+                'description' => json_encode([
+                    'text' => '',
+                    'status' => 0,
+                    'classes' => '',
+                    'styles' => [],
+                    'structure' => [
+                        'text' => ['type' => 'text'],
+                        'status' => ['type' => 'text'],
+                        'classes' => ['type' => 'text'],
+                        'styles' => ['type' => 'json', 'nested' => []],
+                    ],
+                ]),
+                'prefix' => 'admin',
+                'classes' => 'nav-item',
+                'styles' => json_encode([]),
+            ]);
+        }
 
-        MenuItem::factory()->create([
-            'menu_id' => 1,
-            'parent_id' => null,
-            'status' => 1,
-            'url' => 'contacts',
-            'title' => json_encode([
-                'text' => 'Contacts',
+        if (!MenuItem::where('url', 'contacts')->where('menu_id', 1)->first()) {
+            MenuItem::factory()->create([
+                'menu_id' => 1,
+                'parent_id' => null,
                 'status' => 1,
-                'classes' => 'icon i-page nav-item-text--light',
-                'styles' => [],
-                'structure' => [
-                    'text' => ['type' => 'text'],
-                    'status' => ['type' => 'text'],
-                    'classes' => ['type' => 'text'],
-                    'styles' => ['type' => 'json', 'nested' => []],
-                ],
-            ]),
-            'description' => json_encode([
-                'text' => '',
-                'status' => 0,
-                'classes' => '',
-                'styles' => [],
-                'structure' => [
-                    'text' => ['type' => 'text'],
-                    'status' => ['type' => 'text'],
-                    'classes' => ['type' => 'text'],
-                    'styles' => ['type' => 'json', 'nested' => []],
-                ],
-            ]),
-            'prefix' => 'admin',
-            'classes' => 'nav-item',
-            'styles' => json_encode([]),
-        ]);
+                'url' => 'contacts',
+                'title' => json_encode([
+                    'text' => 'Contacts',
+                    'status' => 1,
+                    'classes' => 'icon i-page nav-item-text--light',
+                    'styles' => [],
+                    'structure' => [
+                        'text' => ['type' => 'text'],
+                        'status' => ['type' => 'text'],
+                        'classes' => ['type' => 'text'],
+                        'styles' => ['type' => 'json', 'nested' => []],
+                    ],
+                ]),
+                'description' => json_encode([
+                    'text' => '',
+                    'status' => 0,
+                    'classes' => '',
+                    'styles' => [],
+                    'structure' => [
+                        'text' => ['type' => 'text'],
+                        'status' => ['type' => 'text'],
+                        'classes' => ['type' => 'text'],
+                        'styles' => ['type' => 'json', 'nested' => []],
+                    ],
+                ]),
+                'prefix' => 'admin',
+                'classes' => 'nav-item',
+                'styles' => json_encode([]),
+            ]);
+        }
+
+        if (!MenuItem::where('url', 'request-tracks')->where('menu_id', 1)->first()) {
+            MenuItem::factory()->create([
+                'menu_id' => 1,
+                'parent_id' => null,
+                'status' => 1,
+                'url' => 'request-tracks',
+                'title' => json_encode([
+                    'text' => 'Tracked request',
+                    'status' => 1,
+                    'classes' => 'icon i-page nav-item-text--light',
+                    'styles' => [],
+                    'structure' => [
+                        'text' => ['type' => 'text'],
+                        'status' => ['type' => 'text'],
+                        'classes' => ['type' => 'text'],
+                        'styles' => ['type' => 'json', 'nested' => []],
+                    ],
+                ]),
+                'description' => json_encode([
+                    'text' => '',
+                    'status' => 0,
+                    'classes' => '',
+                    'styles' => [],
+                    'structure' => [
+                        'text' => ['type' => 'text'],
+                        'status' => ['type' => 'text'],
+                        'classes' => ['type' => 'text'],
+                        'styles' => ['type' => 'json', 'nested' => []],
+                    ],
+                ]),
+                'prefix' => 'admin',
+                'classes' => 'nav-item',
+                'styles' => json_encode([]),
+            ]);
+        }
     }
 }
