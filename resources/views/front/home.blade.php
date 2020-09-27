82211fbbdb33c37f46e92ef9e5d8f7f500683aa2
stoyantodorovbg <stoyan_11@abv.bg>
2020-09-27 20:17:26 +0300

    Updated titles

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -6,13 +6,19 @@
         </div>
 
         <h1 class="title">Vega CMS 1.12.4
-            <div class="sub-title">PHP Package</div>
-            <div class="sub-title">based on Laravel Framework</div>
+            <div class="sub-title">Open Sourced PHP Package based on Laravel Framework</div>
         </h1>
         <h3 class="text-center display-3">
-        <a href="https://packagist.org/packages/vegacms/cms#dev-master" target="_blank">
-            Packagist
-        </a>
+            <div>
+                <a href="https://packagist.org/packages/vegacms/cms#dev-master" target="_blank">
+                    Packagist
+                </a>
+            </div>
+            <div>
+                <a href="https://github.com/stoyantodorovbg/Vega-CMS-Package" target="_blank">
+                    Github
+                </a>
+            </div>
         </h3>
         <div class="row">
             <div class="col-12">
