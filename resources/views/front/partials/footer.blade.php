0bef9b4e7cf9d6a4b17ceeafd1dc13a2f0734e61
stoyantodorovbg <stoyan_11@abv.bg>
2020-10-05 21:54:23 +0300

    footer edited

--- resources/views/front/partials/footer.blade.php
+++ resources/views/front/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-    <span>Vega CMS is open-sourced software licensed under the MIT license.</span>
+    <span>Vega CMS PHP Package is an open-sourced software licensed under MIT license.</span>
 </footer>
